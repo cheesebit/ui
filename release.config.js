7601d8e1e938c5cb303cef17ed1036d345dc1366
Welington Silva <welington.manoels@gmail.com>
2020-11-17 00:05:54 -0300

    chore(build): adding semantic release

---
+++ release.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  release: {
+    branches: ['master', 'release'],
+  },
+
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
