a87611d297e714a088cede1ebbc1f3736f3b5f53
Welington Silva <welington.manoels@gmail.com>
2020-11-29 17:36:04 -0300

    fix(ci): attempt to use semantic-release

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   release: {
-    branches: ['master', 'release'],
+    branches: ['release'],
   },
 
   plugins: [
@@ -16,7 +16,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['package.json'],
+        assets: ['docs', 'package.json'],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
