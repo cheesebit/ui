fc99b5a2839d6d9172e4f49503587f71897f94ef
Welington Silva <welington.manoels@gmail.com>
2021-02-22 01:05:12 -0300

    feat: add useChangeable hook

---
+++ src/hooks/changeable/index.js
@@ -0,0 +1,1 @@
+export { default as useChangeable } from './use-changeable'
---
+++ src/hooks/changeable/use-changeable.js
@@ -0,0 +1,20 @@
+import { useCallback, useState } from 'react';
+
+function useChangeable({ onChange: onChangeProp, value: valueProp }) {
+  const [value, setValue] = useState(valueProp);
+
+  const onChange = useCallback(
+    function handleChange(e) {
+      const {
+        target: { value },
+      } = e;
+      setValue(value);
+      onChangeProp?.(e);
+    },
+    [onChangeProp],
+  );
+
+  return [value, onChange];
+}
+
+export default useChangeable;
