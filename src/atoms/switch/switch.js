afaa224dbc85ea4d2721aba1f99c1e56787ee6a3
Welington Silva <welington.manoels@gmail.com>
2021-03-26 17:04:57 -0300

    feat: add switch component

---
+++ src/atoms/switch/switch.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import clsx from 'clsx';
+
+import { Box } from '../box';
+import { Icon } from '../icon';
+
+import './switch.scss';
+
+const Switch = ({
+  borderless = true,
+  children,
+  className,
+  disabled,
+  paddingless = 'horizontal',
+  block = false,
+  trailing,
+  ...others
+}) => {
+  return (
+    <Box
+      as="label"
+      borderless={borderless}
+      paddingless={paddingless}
+      block={block}
+      trailing={trailing}
+      className={clsx('cb-switch', { 'is-disabled': disabled }, className)}
+      data-testid="cb-switch"
+      leading={
+        <React.Fragment>
+          <input
+            {...others}
+            disabled={disabled}
+            type="checkbox"
+            className="selector"
+            data-testid="selector"
+          />
+          <Icon name="circle" className="check" size={14} />
+        </React.Fragment>
+      }
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default Switch;
---
+++ src/atoms/switch/switch.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Switch from './switch';
+import generator from '../../../test/data-generator';
+
+export default {
+  title: 'Atoms/Switch',
+  component: Switch,
+  docs: {
+    description: {
+      story: 'some story *a*markdown**',
+    },
+  },
+};
+
+const Template = args => {
+  return (
+    <div className="block">
+      <p className="mb-2">
+        This is me, a cool Switch family ready to be played around. Try me :)
+      </p>
+      <div className="flex flex-col space-y-2">
+        <Switch {...args}>{generator.name()}</Switch>
+        <Switch {...args}>{generator.name()}</Switch>
+        <Switch {...args}>{generator.name()}</Switch>
+      </div>
+    </div>
+  );
+};
+
+export const Playground = Template.bind({});
