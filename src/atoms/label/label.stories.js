806ffdf96e2ef0e43089a2fab954ff81094dac90
Welington Silva <welington.manoels@gmail.com>
2021-10-31 20:49:42 -0300

    refactor: refactor label component

--- src/atoms/label/label.stories.js
+++ src/atoms/label/label.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Input } from '../input';
+import { Input } from 'atoms/input';
 import Label from './label';
 
 export default {
@@ -15,19 +15,24 @@ export default {
 	},
 };
 
-export function Playground( args ) {
+export function Playground(args) {
 	return (
 		<div className="block">
 			<p className="mb-2">
-				This is me, a cool Label (Field) ready to be played around. Try me :)
+				This is me, a cool Label (Field) ready to be played around. Try
+				me :)
 			</p>
 
-			<Label { ...args } />
+			<div className="flex flex-col space-y-1">
+				<Label {...args} />
+				<Input />
+			</div>
 		</div>
 	);
 }
 
 Playground.args = {
-	label: 'My cool field',
-	children: <Input type="text" />,
+	children: 'My cool field',
+	trailing: <span>&spades;</span>,
+	variant: 'neutral',
 };
--- src/atoms/label/label.test.js
+++ src/atoms/label/label.test.js
@@ -1,342 +1,83 @@
 import React from 'react';
 import { composeStories } from '@storybook/testing-react';
 
-import { Mode, Placement } from '../tooltip';
-import { render, screen, userEvent } from 'test/helpers';
-import { Variant } from './label';
+import { render, screen } from 'test/helpers';
 import * as stories from './label.stories';
 import generator from 'test/data-generator';
-import Selectors, { DEFAULT_FEEDBACK, DEFAULT_TOOLTIP } from './label.selectors';
 
-const { Playground } = composeStories( stories );
+const { Playground } = composeStories(stories);
 
-describe( '<Playground />', () => {
-	it( 'renders correctly', () => {
-		const props = { label: generator.word(), children: generator.word() };
+describe('<Label />', () => {
+	it('renders correctly', () => {
+		const props = { children: generator.word() };
 
-		render( <Playground { ...props } /> );
+		render(<Playground {...props} />);
 
-		const component = screen.getByTestId( 'cb-label' );
-		const label = screen.getByTestId( 'field-label' );
-		const content = screen.getByTestId( 'field-content' );
-		const prompt = screen.getByTestId( 'field-prompt' );
+		const component = screen.getByTestId('cb-label');
+		expect(component).toBeTruthy();
+	});
 
-		expect( component ).toBeTruthy();
-		expect( label ).toBeTruthy();
-		expect( content ).toBeTruthy();
-		expect( prompt ).toBeTruthy();
-	} );
+	it('renders label correctly', () => {
+		const props = { children: generator.word() };
 
-	it( 'renders label correctly', () => {
-		const props = { label: generator.word(), children: generator.word() };
+		render(<Playground {...props} />);
 
-		render( <Playground { ...props } /> );
+		const label = screen.getByTestId('cb-label');
 
-		screen.getByTestId( 'cb-label' );
-		const label = screen.getByTestId( 'field-label' );
+		expect(label).toHaveTextContent(props.children);
+	});
 
-		expect( label ).toHaveTextContent( props.label );
-	} );
-
-	it( 'renders prompt correctly', () => {
-		const props = {
-			label: generator.word(),
-			children: generator.word(),
-			prompt: generator.sentence(),
-		};
-
-		render( <Playground { ...props } /> );
-
-		const prompt = screen.getByTestId( 'field-prompt' );
-
-		expect( prompt ).toHaveTextContent( props.prompt );
-	} );
-
-	it( 'renders content correctly', () => {
-		const props = {
-			label: generator.word(),
-			children: generator.word(),
-		};
-
-		render( <Playground { ...props } /> );
-
-		const component = screen.getByTestId( 'cb-label' );
-
-		expect( component ).toHaveTextContent( props.children );
-	} );
-
-	it( 'renders tooltip correctly', () => {
-		const props = {
-			label: generator.word(),
-			children: generator.word(),
-			trailing: null,
-			tooltip: {
-				icon: 'help',
-				text: generator.sentence(),
-			},
-		};
-
-		render( <Playground { ...props } /> );
-
-		const tooltip = screen.getByTestId( 'cb-tooltip' );
-		const anchor = screen.getByTestId( 'tooltip-anchor' );
-
-		userEvent.hover( anchor );
-
-		expect( tooltip ).toHaveTextContent( props.tooltip.text );
-	} );
-
-	it( 'renders feedback correctly', () => {
-		const props = {
-			label: generator.word(),
-			children: generator.word(),
-			feedback: {
-				icon: 'help',
-				text: generator.sentence(),
-			},
-		};
-
-		render( <Playground { ...props } /> );
-
-		const prompt = screen.getByTestId( 'field-prompt' );
-		const icon = screen.getByLabelText( props.feedback.icon );
-
-		expect( icon ).toBeInTheDocument();
-		expect( prompt ).toHaveTextContent( props.feedback.text );
-	} );
-
-	it( 'renders empty prompt when no feedback text is provided', () => {
-		const props = {
-			label: generator.word(),
-			children: generator.word(),
-			feedback: {
-				icon: 'help',
-			},
-		};
-
-		render( <Playground { ...props } /> );
-
-		const prompt = screen.getByTestId( 'field-prompt' );
-		const icon = screen.getByLabelText( props.feedback.icon );
-
-		expect( icon ).toBeInTheDocument();
-		expect( prompt ).toBeEmptyDOMElement();
-	} );
-
-	it( 'overrides tooltip when feedback prop is provided', () => {
-		const props = {
-			label: generator.word(),
-			children: generator.word(),
-			prompt: generator.sentence(),
-			tooltip: {
-				icon: 'help',
-				text: generator.sentence(),
-			},
-		};
-
-		const { rerender } = render(
-			<Playground { ...props } />,
-		);
-
-		const prompt = screen.getByTestId( 'field-prompt' );
-		const tooltip = screen.getByTestId( 'cb-tooltip' );
-		let icon = screen.getByLabelText( props.tooltip.icon );
-
-		expect( prompt ).toHaveTextContent( props.prompt );
-		expect( tooltip ).toHaveTextContent( props.tooltip.text );
-		expect( icon ).toBeInTheDocument();
-
-		const addedProps = {
-			feedback: {
-				icon: 'search',
-				text: generator.sentence(),
-			},
-		};
-
-		rerender( <Playground { ...props } { ...addedProps } /> );
-
-		icon = screen.getByLabelText( addedProps.feedback.icon );
-
-		expect( prompt ).toHaveTextContent( addedProps.feedback.text );
-		expect( tooltip ).toHaveTextContent( addedProps.feedback.text );
-		expect( icon ).toBeInTheDocument();
-	} );
-
-	it( 'shows tooltip when feedback prop is cleaned', () => {
-		const props = {
-			label: generator.word(),
-			children: generator.word(),
-			prompt: generator.sentence(),
-			tooltip: {
-				icon: 'help',
-				text: generator.sentence(),
-			},
-			feedback: {
-				icon: 'search',
-				text: generator.sentence(),
-			},
-		};
-
-		const { rerender } = render(
-			<Playground { ...props } />,
-		);
-
-		const prompt = screen.getByTestId( 'field-prompt' );
-		const tooltip = screen.getByTestId( 'cb-tooltip' );
-		let icon = screen.getByLabelText( props.feedback.icon );
-
-		expect( prompt ).toHaveTextContent( props.feedback.text );
-		expect( tooltip ).toHaveTextContent( props.feedback.text );
-		expect( icon ).toBeInTheDocument();
-
-		const addedProps = {
-			feedback: {},
-		};
-
-		rerender( <Playground { ...props } { ...addedProps } /> );
-
-		icon = screen.getByLabelText( props.tooltip.icon );
-
-		expect( prompt ).toHaveTextContent( props.prompt );
-		expect( tooltip ).toHaveTextContent( props.tooltip.text );
-		expect( icon ).toBeInTheDocument();
-	} );
-
-	it( 'renders custom trailing correctly', () => {
-		const props = {
-			label: generator.word(),
-			children: generator.word(),
-			trailing: generator.animal(),
-		};
-
-		render( <Playground { ...props } /> );
-
-		const content = screen.getByTestId( 'field-label' );
-
-		expect( content ).toHaveTextContent( props.trailing );
-	} );
-
-	describe( 'with variant', () => {
-		it( `renders correctly with variant ${ Variant.danger }`, () => {
+	describe('with variant', () => {
+		it(`renders correctly with variant danger`, () => {
 			const props = {
 				label: generator.word(),
 				children: generator.word(),
-				variant: Variant.danger,
+				variant: 'danger',
 			};
 
-			render( <Playground { ...props } /> );
+			render(<Playground {...props} />);
 
-			const component = screen.getByTestId( 'cb-label' );
-			expect( component ).toHaveClass( '-danger' );
-		} );
+			const component = screen.getByTestId('cb-label');
+			expect(component).toHaveClass('-danger');
+		});
 
-		it( `renders correctly with variant ${ Variant.info }`, () => {
+		it(`renders correctly with variant info`, () => {
 			const props = {
 				label: generator.word(),
 				children: generator.word(),
-				variant: Variant.info,
+				variant: 'info',
 			};
 
-			render( <Playground { ...props } /> );
+			render(<Playground {...props} />);
 
-			const component = screen.getByTestId( 'cb-label' );
-			expect( component ).toHaveClass( '-info' );
-		} );
+			const component = screen.getByTestId('cb-label');
+			expect(component).toHaveClass('-info');
+		});
 
-		it( `renders correctly with variant ${ Variant.success }`, () => {
+		it(`renders correctly with variant success`, () => {
 			const props = {
 				label: generator.word(),
 				children: generator.word(),
-				variant: Variant.success,
+				variant: 'success',
 			};
 
-			render( <Playground { ...props } /> );
+			render(<Playground {...props} />);
 
-			const component = screen.getByTestId( 'cb-label' );
-			expect( component ).toHaveClass( '-success' );
-		} );
+			const component = screen.getByTestId('cb-label');
+			expect(component).toHaveClass('-success');
+		});
 
-		it( `renders correctly with variant ${ Variant.warn }`, () => {
+		it(`renders correctly with variant warn`, () => {
 			const props = {
 				label: generator.word(),
 				children: generator.word(),
-				variant: Variant.warn,
+				variant: 'warn',
 			};
 
-			render( <Playground { ...props } /> );
+			render(<Playground {...props} />);
 
-			const component = screen.getByTestId( 'cb-label' );
-			expect( component ).toHaveClass( '-warn' );
-		} );
-	} );
-} );
-
-describe( 'Label Selectors', () => {
-	it( 'retrieves empty feedback prop correctly', () => {
-		const props = {
-			feedback: {},
-		};
-
-		expect( Selectors.getFeedback( props ) ).toEqual( {} );
-	} );
-
-	it( 'retrieves default feedback merged with prop correctly', () => {
-		const props = {
-			feedback: {
-				text: generator.sentence(),
-			},
-		};
-
-		expect( Selectors.getFeedback( props ) ).toEqual( {
-			...DEFAULT_FEEDBACK,
-			...props.feedback,
-		} );
-	} );
-
-	it( 'retrieves feedback prop correctly', () => {
-		const props = {
-			feedback: {
-				mode: Mode.light,
-				position: Placement.right,
-				text: generator.sentence(),
-			},
-		};
-
-		expect( Selectors.getFeedback( props ) ).toEqual( props.feedback );
-	} );
-
-	it( 'retrieves empty tooltip prop correctly', () => {
-		const props = {
-			tooltip: {},
-		};
-
-		expect( Selectors.getTooltip( props ) ).toEqual( {} );
-	} );
-
-	it( 'retrieves default tooltip merged with prop correctly', () => {
-		const props = {
-			tooltip: {
-				text: generator.sentence(),
-			},
-		};
-
-		expect( Selectors.getTooltip( props ) ).toEqual( {
-			...DEFAULT_TOOLTIP,
-			...props.tooltip,
-		} );
-	} );
-
-	it( 'retrieves tooltip prop correctly', () => {
-		const props = {
-			tooltip: {
-				mode: Mode.light,
-				position: Placement.right,
-				text: generator.sentence(),
-			},
-		};
-
-		expect( Selectors.getTooltip( props ) ).toEqual( props.tooltip );
-	} );
-
-	// it( 'retrieves prompt prop correctly', () => {} );
-} );
+			const component = screen.getByTestId('cb-label');
+			expect(component).toHaveClass('-warn');
+		});
+	});
+});
