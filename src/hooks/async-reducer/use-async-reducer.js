d6695f3fe84fcadf45e09c918f0396f008455a8c
Welington Silva <welington.manoels@gmail.com>
2021-10-31 20:58:55 -0300

    refactor: refactor async reducer hook

--- src/hooks/async-reducer/use-async-reducer.js
+++ src/hooks/async-reducer/use-async-reducer.js
@@ -1,31 +1,36 @@
 import React from 'react';
 
-import { isFunction } from '../../common/toolset';
-
 /**
  * Applies thunk approach to default React's `useReducer`.
  * In a nutshell, you can dispatcher either action object or function, which receives
  * as argument the dispatch.
  *
- * @param {Function} reducer
- * @param {*} initializerArg
- * @param {Function} initializer
+ * @template S, A
+ * @param {React.Reducer<S, A>} reducer
+ * @param {S} initializerArg
+ * @param {(arg: any) => S} [initializer]
+ * @return {[S, (action: A | ((dispatch: (value: A) => void) => void)) => void]}
  */
-export default function useAsyncReducer( reducer, initializerArg, initializer ) {
-	const [ state, dispatch ] = React.useReducer(
+export default function useAsyncReducer(reducer, initializerArg, initializer) {
+	const [state, dispatch] = React.useReducer(
 		reducer,
 		initializerArg,
-		initializer,
+		initializer
 	);
 
-	function thunkenize( action ) {
-		if ( isFunction( action ) ) {
-			action( dispatch );
+	/**
+	 *
+	 * @param {A | ((dispatch: (value: A) => void) => void)} action
+	 * @returns
+	 */
+	function thunkenize(action) {
+		if (action instanceof Function) {
+			action(dispatch);
 			return;
 		}
 
-		dispatch( action );
+		dispatch(action);
 	}
 
-	return [ state, thunkenize ];
+	return [state, thunkenize];
 }
