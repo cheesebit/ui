aa5627c71674ca7082049efe9dfd6ac29f92810b
Welington Silva <welington.manoels@gmail.com>
2021-10-31 18:27:56 -0300

    chore: config improvements

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [ 'plugin:@wordpress/eslint-plugin/recommended-with-formatting' ],
-	parserOptions: {
-		ecmaVersion: 12,
-		sourceType: 'module',
-		project: [ './jsconfig.json' ],
-	},
-	rules: {
-		'no-shadow': 'warn',
-		'no-unused-expressions': 'warn',
-		eqeqeq: 0,
-	},
-	settings: {
-		'import/resolver': {
-			node: {
-				extensions: [ '.js', '.jsx', '.ts', '.tsx' ],
-				moduleDirectory: [ 'node_modules', '.',
-				],
-			},
-		},
-	},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:@wordpress/eslint-plugin/recommended-with-formatting"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    project: ["./jsconfig.json"],
+  },
+  rules: {
+    "no-shadow": "warn",
+    "no-unused-expressions": "warn",
+    "no-unused-vars": "warn",
+    "jsdoc/valid-types": "warn",
+    "jsx-a11y/aria-props": "warn", // well, none and presentation are real valir roles
+    eqeqeq: 0,
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        moduleDirectory: ["node_modules", "."],
+      },
+    },
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-async-generator-functions',
-    'ramda',
-    [
-      require.resolve('babel-plugin-module-resolver'),
-      {
-        root: ['./'],
-        alias: {
-          test: './test',
-          common: './src/common',
-          styles: './src/styles',
-          atoms: './src/atoms',
-          molecules: './src/molecules',
-          organisms: './src/organisms',
-          hocs: './src/hocs',
-          hooks: './src/hooks',
-        },
-      },
-    ],
-  ],
+	presets: [ '@babel/preset-env', '@babel/preset-react' ],
+	plugins: [
+		'@babel/plugin-proposal-class-properties',
+		'@babel/plugin-proposal-optional-chaining',
+		'@babel/plugin-proposal-nullish-coalescing-operator',
+		'@babel/plugin-proposal-async-generator-functions',
+		'ramda',
+		[
+			require.resolve( 'babel-plugin-module-resolver' ),
+			{
+				root: [ './' ],
+				alias: {
+					test: './test',
+					common: './src/common',
+					styles: './src/styles',
+					atoms: './src/atoms',
+					molecules: './src/molecules',
+					organisms: './src/organisms',
+					hocs: './src/hocs',
+					hooks: './src/hooks',
+				},
+			},
+		],
+	],
 };
