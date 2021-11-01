86c5dd2469400f381e02ae6eb83a038e24764ee2
Welington Silva <welington.manoels@gmail.com>
2021-10-31 21:10:33 -0300

    fix: deps and lint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,20 +1,16 @@
 module.exports = {
-	plugins: [ 'stylelint-scss', 'stylelint-order' ],
-	extends: [ 'stylelint-config-recommended', 'stylelint-config-rational-order', 'stylelint-config-prettier' ],
-	ignoreFiles: [
-		'node_modules/',
-		'**/*.json',
+	plugins: ['stylelint-order'],
+	extends: [
+		'stylelint-config-standard-scss',
+		'stylelint-config-rational-order',
+		'stylelint-config-prettier',
 	],
+	ignoreFiles: ['node_modules/', '**/*.json'],
 	rules: {
-		'order/order': [
-			'dollar-variables',
-			'declarations',
-		],
-		'plugin/rational-order': [ true, {
-			'border-in-box-model': false,
-			'empty-line-between-groups': true,
-		} ],
+		'color-function-notation': 'legacy',
 		'at-rule-no-unknown': null,
 		'scss/at-rule-no-unknown': true,
+		'selector-class-pattern': null,
+		'scss/at-import-partial-extension': null,
 	},
 };
